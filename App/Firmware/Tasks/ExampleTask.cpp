3d802683688e2c90a6552e3d9b3a9075a027282b
DarKreter <darkreter@gmail.com>
2022-04-18 17:57:39 +0200

    Add ExampleTask

---
+++ App/Firmware/Tasks/ExampleTask.cpp
@@ -0,0 +1,11 @@
+#include "ExampleTask.hpp"
+
+// Create a task named "ExampleTask" with priority 3, periodicity 1000ms and custom
+// stack size 256.
+ExampleTask::ExampleTask()
+    : SBT::System::PeriodicTask("ExampleTask", 3, 1000, 256) {}
+
+// This task does not need any initialization
+void ExampleTask::initialize() {}
+
+void ExampleTask::run() {}
